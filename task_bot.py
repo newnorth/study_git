fbd97675ba4fade9021929f93a70e586918cf819
Eugene Linkevich <linkevich.ev@yandex.ru>
2021-11-21 10:20:35 +0300

    task_bot.py fix /show

--- task_bot.py
+++ task_bot.py
@@ -1,51 +1,9 @@
 import telebot
 import random
 
-token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 bot = telebot.TeleBot(token)
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, help_list)
-
-@bot.message_handler(commands=['add'])
-def add(message):
-    command = message.text.split(maxsplit=2)
-    date = command[1].lower()
-    task = command[2]
-    todo_list(date, task)
-    text = 'Задача ' + task + ' добавлена на дату ' + date
-    bot.send_message(message.chat.id, text)
-
-
-@bot.message_handler(commands=['random'])
-def random_add(message):
-    date = 'сегодня'
-    task = random.choice(random_list)
-    todo_list(date, task)
-    text = 'Задача ' + task + ' добавлена на дату ' + date
-    bot.send_message(message.chat.id, text)
-
-@bot.message_handler(commands=['show', 'print'])
-def show(message):
-    command = message.text.split(maxsplit=1)
-    date = command[0].lower()
-    text = ''
-    if date in tasks:
-        text = date.upper() + '\n'
-        for task in tasks[date]:
-            text = text + '[]' + task + '\n'
-    else:
-        text = 'Задач на эту дату нет.'
-    bot.send_message(message.chat.id, text)
-
-def todo_list(time_date, task):
-    if time_date in tasks:
-        tasks[time_date].append(task)
-    else:
-        tasks[time_date] = []
-        tasks[time_date].append(task)
 
 help_list = """ 
 /help - напечатать справку по программе
@@ -54,8 +12,64 @@ help_list = """
 /random - добавляет случайную задачу текущий день
 /exit - выход
 """
+
 random_list = ['Изучить алгоритмы', 'Выучить SQL', 'Найти работу']
 
 tasks = {}
 
+
+def add_todo(date, task):
+    if date in tasks:
+        tasks[date].append(task)
+    else:
+        tasks[date] = []
+        tasks[date].append(task)
+
+
+# вывод списка команд
+@bot.message_handler(commands=['help'])
+def help(message):
+    print(message.text)
+    bot.send_message(message.chat.id, help_list)
+
+
+@bot.message_handler(commands=['add'])
+def add(message):
+    print(message.text)
+    command = message.text.split(maxsplit=2)
+    date = command[1].lower()
+    task = command[2]
+    add_todo(date, task)
+    output = 'Задача ' + task + ' добавлена на дату ' + date
+    bot.send_message(message.chat.id, output)
+
+
+@bot.message_handler(commands=['random'])
+def random_add(message):
+    print(message.text)
+    date = 'сегодня'
+    task = random.choice(random_list)
+    add_todo(date, task)
+    output = 'Задача ' + task + ' добавлена на дату ' + date
+    bot.send_message(message.chat.id, output)
+
+
+@bot.message_handler(commands=['show'])
+def show(message):
+    print(message.text)
+    command = message.text.split(maxsplit=1)
+    text = ''
+    if len(command) == 1:
+        text = 'Укажите дату.'
+    else:
+        date = command[1].lower()
+        if date in tasks:
+            text = date.upper() + '\n'
+            for task in tasks[date]:
+                text = text + '[]' + task + '\n'
+        else:
+            text = 'Задач на эту дату нет.'
+    bot.send_message(message.chat.id, text)
+
+# Постоянное обращение к серверам телеграмма
 bot.polling(none_stop=True)
